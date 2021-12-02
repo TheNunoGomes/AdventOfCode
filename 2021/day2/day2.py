6ae2bc9c419f5133c22a439a2fb8382fe73248f3
NunoGomes <nunomrgomes@gmail.com>
2021-12-02 19:00:44 +0000

    feat: 2021 day 2

---
+++ 2021/day2/day2.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def calculateDepth(data):
+    return sum([instruction[1] for instruction in data if instruction[0] == "forward"]), sum([instruction[1] for instruction in data if instruction[0] == "down"]) - sum([instruction[1] for instruction in data if instruction[0] == "up"])
+
+
+def calculateDepthWithAim(data):
+    distance, depth, aim = 0, 0, 0
+    for instruction in data:
+        direction, value = instruction
+        if direction == "forward":
+            distance += value
+            depth += value*aim
+        elif direction == "up":
+            aim -= value
+        elif direction == "down":
+            aim += value
+            
+    return distance, depth
+
+data = np.genfromtxt('data.txt', dtype = None, encoding = None)
+
+distance1, depth1 = calculateDepth(data)
+distance2, depth2 = calculateDepthWithAim(data)
+
+print(distance1*depth1)
+print(distance2*depth2)
