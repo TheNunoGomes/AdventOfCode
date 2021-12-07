a69837bb12c34290957314ba63776d72d131d345
NunoGomes <nunomrgomes@gmail.com>
2021-12-07 20:35:16 +0000

    refactor: optimized day 7 2021 pt2 by calculating the ideal position using derivatives

--- 2021/day7/day7.py
+++ 2021/day7/day7.py
@@ -1,23 +1,24 @@
+def average(data):
+    return sum(data)/len(data)
+
+def idealPosition(data, positive = False):
+    return round(sum(data)/len(data) + (0.5 if positive else -0.5))
 
 def distanceConsumptionConstant(data, target):
     return sum([abs(position-target) for position in data])
     
 def distanceConsumptionArithmetic(data, target):    
     return sum([sum([i for i in range(1, abs(position-target)+1)]) for position in data])
-    
 
 def alignCrabsConstant(data):
     return min([distanceConsumptionConstant(data, target) for target in range(0, max(data))])
 
 def alignCrabsArithmetic(data):
-    return min([distanceConsumptionArithmetic(data, target) for target in range(0, max(data))])
-
+    return min([distanceConsumptionArithmetic(data, idealPosition(data, True)), distanceConsumptionArithmetic(data, idealPosition(data))])
 
 with open('data.txt') as f:
     data = f.readlines()
     data = [int(crab) for crab in data[0].split(',')]
- 
+
 print(alignCrabsConstant(data))
 print(alignCrabsArithmetic(data))
-
-
