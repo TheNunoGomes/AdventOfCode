e23b73d928e3494e72c4b43128ed453cf760ba15
Nuno Gomes <nunomrgomes@gmail.com>
2022-12-01 22:56:18 +0000

    feat: day 1 datasets and solution

---
+++ 2022/days/day1.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def splitElfCalories(data):
+    return [[int(calories) for calories in elf] for elf in [calories.split(' ') for calories in ' '.join(data).split('  ')]]
+
+def countCaloriesByElf(data):
+    return [sum(elfCalories) for elfCalories in splitElfCalories(data)]
+
+
+def part1(data):
+    print("Day 1 | Part 1")
+    maxCalories = 0
+    currentCalories = 0
+    for calories in data:
+        maxCalories = max(currentCalories, maxCalories)
+        currentCalories = currentCalories + int(calories) if calories.isdigit() else 0
+
+    return maxCalories
+
+def part2(data):
+    print("Day 1 | Part 2")
+
+    caloriesByElf = countCaloriesByElf(data)
+    maxCalories = 0
+
+    for i in range(0, 3):
+        currentMaxCalories = max(caloriesByElf)
+        maxCalories += currentMaxCalories
+        caloriesByElf.pop(caloriesByElf.index(currentMaxCalories))
+    
+    return maxCalories
