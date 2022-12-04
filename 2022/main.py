704d6bb50a710aae676f167e7eb9d08560a4c2df
Nuno Gomes <nunomrgomes@gmail.com>
2022-12-04 13:08:54 +0000

    refactor: changed dataset naming convention

--- 2022/main.py
+++ 2022/main.py
@@ -5,9 +5,8 @@ day, part, test = argParser(sys.argv[1:])
 
 exec(f"from solutions.day{day} import part{part} as solve")
 
-with open(f"datasets/day{day}_{'test' if test else 'challenge'}.txt") as f:
-    data = f.readlines()
-    data = [line.replace('\n', '') for line in data]
+with open(f"datasets/day{day}{'_t' if test else ''}.in") as f:
+    data = [line.replace('\n', '') for line in f.readlines()]
 
 print(f"Day {day} | Part {part} | {'Test' if test else 'Challenge'} dataset")
 print(solve(data))
