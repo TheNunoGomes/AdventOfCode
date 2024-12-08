630f18963d31ee25ef77310beeb04f4aae154929
Nuno Gomes <nunomrgomes@hotmail.com>
2024-12-08 21:58:03 +0000

    day 6 p 2 incomplete

--- 2024/solutions/day6.js
+++ 2024/solutions/day6.js
@@ -11,6 +11,7 @@ const directions = {
   v: [0, 1],
   "<": [-1, 0],
 };
+
 const turnDirections = {
   "^": ">",
   ">": "v",
@@ -71,12 +72,30 @@ function moveGuard(data, currentPosition, currentGuardDirection) {
   const targetPosition = [x + dx, y + dy];
   const [tx, ty] = targetPosition;
 
-  data[y] = replaceCharacter(data[y], "X", x);
+  const replacementChar = Number(data[y][x])
+    ? `${Number(data[y][x]) + 1}`
+    : "1";
 
+  if (replacementChar === 4) {
+    return true;
+  }
+
+  data[y] = replaceCharacter(data[y], replacementChar, x);
   data[ty] = replaceCharacter(data[ty], currentGuardDirection, tx);
   return targetPosition;
+}
 
-  return false;
+function tickGuard(data, guardPosition, guardDirection) {
+  let i = 0;
+  // Turn guard until he's not facing an object
+  while (nextPositionIsObstacle(data, guardPosition, guardDirection)) {
+    i++;
+    guardDirection = turnGuard(data, guardPosition, guardDirection);
+    // To avoid infinite spins
+    if (i > 10) return "Spinning forever";
+  }
+  guardPosition = moveGuard(data, guardPosition, guardDirection);
+  return [guardPosition, guardDirection];
 }
 
 function part1(data) {
@@ -87,28 +106,86 @@ function part1(data) {
   let guardPosition = initialGuardPosition;
   let guardDirection = initialGuardDirection;
 
-  printMap(data);
   do {
-    let i = 0;
-    // Turn guard until he's not facing an object
-    while (nextPositionIsObstacle(data, guardPosition, guardDirection)) {
-      i++;
-      guardDirection = turnGuard(data, guardPosition, guardDirection);
-      // To avoid infinite spins
-      if (i > 10) return "Spinning forever";
-    }
-    guardPosition = moveGuard(data, guardPosition, guardDirection);
+    [guardPosition, guardDirection] = tickGuard(
+      data,
+      guardPosition,
+      guardDirection
+    );
   } while (nextPositionIsInBounds(data, guardPosition, guardDirection)); // moveGuard will return false once the guard is outside the map
   const finalGuardPosition = guardPosition;
   const [x, y] = finalGuardPosition;
   data[y] = replaceCharacter(data[y], "X", x);
-  printMap(data);
   return data.join("\n").replace(/[^X]/g, "").length;
 }
 
-function part2(data) {}
+async function part2(data) {
+  let loopsFound = 0;
+  const realMap = [...data];
+
+  let guardPosition = findGuard(realMap);
+  const [ix, iy] = guardPosition;
+  let guardDirection = realMap[iy][ix];
+
+  do {
+    console.log("Real Map");
+    printMap(realMap);
+
+    await waitForInput("");
+    const loopCheckMap = [...realMap];
+    let loopCheckPosition = guardPosition;
+    let loopCheckDirection = guardDirection;
+
+    // Check if placing an obstacle in front causes a loop
+    do {
+      console.log("Loop Map");
+      printMap(loopCheckMap);
+      await waitForInput("");
+      const tick = tickGuard(
+        loopCheckMap,
+        loopCheckPosition,
+        loopCheckDirection
+      );
+      if (tick === true) {
+        loopsFound++;
+        break;
+      }
+
+      [loopCheckPosition, loopCheckDirection] = tick;
+    } while (
+      nextPositionIsInBounds(
+        loopCheckMap,
+        loopCheckPosition,
+        loopCheckDirection
+      )
+    ); // moveGuard will return false once the guard is outside the map
+
+    [guardPosition, guardDirection] = tickGuard(
+      realMap,
+      guardPosition,
+      guardDirection
+    );
+  } while (nextPositionIsInBounds(realMap, guardPosition, guardDirection)); // moveGuard will return false once the guard is outside the map
+  const finalGuardPosition = guardPosition;
+  const [x, y] = finalGuardPosition;
+  realMap[y] = replaceCharacter(realMap[y], "X", x);
+  return loopsFound;
+}
 
 module.exports = {
   part1,
   part2,
+};
+
+const waitForInput = (query) => {
+  return new Promise((resolve) => {
+    process.stdout.write(query); // Display the query message
+    process.stdin.resume(); // Start listening for input
+    process.stdin.setEncoding("utf8"); // Ensure the input is a string
+
+    process.stdin.on("data", (data) => {
+      process.stdin.pause(); // Stop listening for input
+      resolve(data.trim()); // Resolve the promise with trimmed input
+    });
+  });
 };
